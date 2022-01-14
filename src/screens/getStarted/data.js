b37b703ccb4fe35e61633cf5194ac9d186b4910f
YOKESH K S <ksyokesh98@gmail.com>
2022-01-14 21:44:39 +0530

    onboarding all screens completed (#4)

--- src/screens/getStarted/data.js
+++ src/screens/getStarted/data.js
@@ -4,20 +4,20 @@ export default [
     title: 'Identify Plants',
     description:
       "You can identify the plants you don't know through your camera",
-    image: '../../../assets/getStarted1.png',
+    image: require('../../../assets/getStarted1.png'),
   },
   {
     id: 1,
     title: 'Learn Many Plants Species',
     description:
       "Let's learn about the many plant species that exist in this world",
-    image: '../../../assets/getStarted2.png',
+    image: require('../../../assets/getStarted2.png'),
   },
   {
     id: 2,
     title: 'Read Many Articles About Plant',
     description:
       "Let's learn more about beautiful plants and read many articles about plants and gardening",
-    image: '../../../assets/getStarted3.png',
+    image: require('../../../assets/getStarted3.png'),
   },
 ];
