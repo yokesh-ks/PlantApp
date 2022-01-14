a87fab8cb94a828936d6045f133b113431839a98
yokesh-ks <ksyokesh98@gmail.com>
2022-01-14 13:58:22 +0530

    App initial Setup Files

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Plant App</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default App;
